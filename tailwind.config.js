0eeb2ac30f2b2c31d3e679fbf28ccf9e76425d02
Marcin Mroz <marcin.mroz011@gmail.com>
2023-08-19 00:10:37 +0300

    Contact / Footer / Tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#111",
+        grey: "#f5f0ec",
+        green: "#1b5b31",
+        beige: "#dcc1ab",
+      },
+    },
+  },
+  plugins: [],
+};
