c92071b771c1a0042383b950eea02d617b9d4df6
Marcin Mroz <marcin.mroz011@gmail.com>
2023-08-21 21:39:30 +0300

    Projects Masonry / Gallery overlay

--- script.js
+++ script.js
@@ -22,3 +22,52 @@ window.onclick = function (event) {
 const toggleSearchbar = () => {
   document.getElementById("search-bar").classList.toggle("show");
 };
+const toggleHamburgerSearchbar = () => {
+  document.getElementById("search-bar-ham").classList.toggle("show");
+};
+//Hamburger Menu
+const toggleHamburger = () => {
+  document.getElementById("links").classList.toggle("expand");
+};
+
+//Gallery Popup adapted from https://codepen.io/lave0172/pen/xqQryG
+
+let thumbnails = document.getElementsByClassName("thumbnail");
+let overlay = document.getElementById("gallery-overlay");
+let overlayClose = document.getElementById("overlay-close");
+let overlayImage = document.getElementById("overlay-img");
+
+for (let i = 0; i < thumbnails.length; i++) {
+  //    console.log(thumbnails[i].src)
+  thumbnails[i].addEventListener("click", openOverlay);
+}
+
+overlayClose.addEventListener("click", closeOverlay);
+
+overlay.addEventListener("keydown", overlay);
+overlayClose.addEventListener("click", closeOverlay);
+
+function closeOverlay(event) {
+  if (event.keyCode !== 27) {
+    return;
+  }
+  overlay.classList.toggle("open");
+}
+
+function closeOverlay(event) {
+  overlay.classList.toggle("open");
+}
+
+function openOverlay(event) {
+  overlay.classList.toggle("open");
+  overlayImage.src = event.target.src;
+}
+
+//Masonry initialisation
+const elem = document.querySelector(".grid");
+const masonry = new Masonry(elem, {
+  itemSelector: ".grid-item",
+
+  gutter: 43,
+  fitWidth: true,
+});
